14e1c26f5376ec025ea45fc2679ada8fdf09f0f8
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-13 21:20:48 +0800

    sample basis for chart and json file reading

---
+++ samples/index.js
@@ -0,0 +1,55 @@
+var data;
+
+window.onload = function(){ 
+    document.getElementById('import').onclick = function() {
+        var files = document.getElementById('selectFiles').files;
+        console.log(files);
+        if (files.length <= 0) {
+            return false;
+        }
+      
+        var fr = new FileReader();
+        
+        fr.onload = function(e) { 
+            console.log(e);
+            var result = JSON.parse(e.target.result);
+            data = result;
+        }
+        
+        fr.readAsText(files.item(0));
+    };
+
+    document.getElementById('something').onclick = function() {
+        console.log(data.species_sales);
+        let species_sales = data.species_sales;
+        var properties = Object.keys(species_sales);
+        console.log(properties);
+        var vals = Object.keys(species_sales).map(function(key) {
+            return species_sales[key];
+        });
+
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: properties,
+                datasets: [{
+                    label: 'Species Sales',
+                    data: vals
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    };
+    
+};
+
