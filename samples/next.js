bfdcf0cb82823468cd2614a5a13afc76e739556c
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-15 18:39:35 +0800

    minor color fixes

--- samples/next.js
+++ samples/next.js
@@ -25,13 +25,13 @@ window.onload = function(){
                     label: 'Species Sales',
                     data: vals,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(123, 222, 10, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(123, 222, 10, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -73,9 +73,9 @@ window.onload = function(){
                     label: 'Sales By Burger',
                     data: vals1,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
