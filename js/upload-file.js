abf30365d95a881a4410da77fb3dea36753ca589
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-18 20:27:03 +0800

    basic stuff to upload

--- js/upload-file.js
+++ js/upload-file.js
@@ -1,20 +1,19 @@
-window.onload = function(){ 
-    document.getElementById('import').onclick = function() {
-        var files = document.getElementById('selectFiles').files;
-      console.log(files);
-      if (files.length <= 0) {
-        return false;
-      }
+// window.onload = function(){ 
+//     document.getElementById('import').onclick = function() {
+//         var files = document.getElementById('selectFiles').files;
+//       console.log(files);
+//       if (files.length <= 0) {
+//         return false;
+//       }
       
-      var fr = new FileReader();
+//       var fr = new FileReader();
       
-      fr.onload = function(e) { 
-      console.log(e);
-        var result = JSON.parse(e.target.result);
-        var formatted = JSON.stringify(result, null, 2);
-            document.getElementById('result').value = formatted;
-      }
+//       fr.onload = function(e) { 
+//       console.log(e);
+//         var result = JSON.parse(e.target.result);
+//         var formatted = JSON.stringify(result, null, 2);
+//       }
       
-      fr.readAsText(files.item(0));
-    };
-};
+//       fr.readAsText(files.item(0));
+//     };
+// };
