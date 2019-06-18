697cb5fe97f8c9fa6eb8973c8cca01cd192b3def
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-18 21:40:56 +0800

    upload file page done

--- js/upload-file.js
+++ js/upload-file.js
@@ -1,19 +1,43 @@
-// window.onload = function(){ 
-//     document.getElementById('import').onclick = function() {
-//         var files = document.getElementById('selectFiles').files;
-//       console.log(files);
-//       if (files.length <= 0) {
-//         return false;
-//       }
-      
-//       var fr = new FileReader();
-      
-//       fr.onload = function(e) { 
-//       console.log(e);
-//         var result = JSON.parse(e.target.result);
-//         var formatted = JSON.stringify(result, null, 2);
-//       }
-      
-//       fr.readAsText(files.item(0));
-//     };
-// };
+window.onload = function(){ 
+  document.getElementById('continue').onclick = function() {
+    var files = document.getElementById('selectFiles').files;
+    console.log(files);
+    if (files.length <= 0) {
+        return false;
+    }
+  
+    var fr = new FileReader();
+    
+    fr.onload = function(e) { 
+        console.log(e);
+        var result = JSON.parse(e.target.result);
+        var formatted = JSON.stringify(result, null, 2);
+        sessionStorage.setItem('data', formatted);
+        window.location.href = "dashboard.html";
+    }
+    
+    fr.readAsText(files.item(0));
+  };
+
+    var inputs = document.querySelectorAll( '.upload__input' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+      var label	 = input.nextElementSibling,
+      labelVal = label.innerHTML;
+
+      input.addEventListener( 'change', function( e )
+      {
+        var fileName = '';
+        if( this.files && this.files.length > 1 )
+          fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+        else
+          fileName = e.target.value.split( "\\" ).pop();
+
+        if( fileName ){
+          label.querySelector( 'span' ).innerHTML = fileName;
+        }else{
+          label.innerHTML = labelVal;
+        }
+      });
+    });
+};
