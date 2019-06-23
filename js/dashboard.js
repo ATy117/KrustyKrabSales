fec4c412881812ec63529826efb6e69a385ca137
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-23 20:13:36 +0800

    added ids

---
+++ js/dashboard.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    $('#show-data').click(function() {
+        
+    });
+
+    $('#clear-data').click(function() {
+        
+    });
+
+    $('#log-out').click(function() {
+        
+    });
+};
