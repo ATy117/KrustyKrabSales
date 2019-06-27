1479acf0a1a49c2821529d2042c398dceed2075c
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-06-27 17:46:53 +0800

    big change pareh

---
+++ js/dash-board.js
@@ -0,0 +1,16 @@
+const data = JSON.parse(sessionStorage.getItem('data'));
+const species_sales = data.species_sales;
+const burger_sales = data.burger_sales;
+const burger_by_species = data.burger_by_species;
+const sales = data.sales;
+
+const species_properties = Object.keys(species_sales);
+const burger_properties = Object.keys(burger_sales);
+
+Chart.defaults.global.defaultFontColor = '#FFF';
+
+window.onload = function(){
+
+
+
+};
