3e6154faf2be0744f9a611c471ca408337fa68dd
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-07-11 10:02:15 +0800

    added clear data and log out

--- js/dash-board.js
+++ js/dash-board.js
@@ -54,6 +54,16 @@ window.onload = function(){
         ]
     } );
 
+    $('#clear-data').click(function() {
+        sessionStorage.clear();
+        window.location.href="../html/upload.html";
+    });
+
+    $('#log-out').click(function() {
+        sessionStorage.clear();
+        window.location.href="../html/home.html";
+    });
+
 };
 
 // function clearCanvas(){
